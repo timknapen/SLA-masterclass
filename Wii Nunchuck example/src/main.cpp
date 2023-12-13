d428867acebce83c600e709f1686c269207249f0
tim knapen <timknapen@gmail.com>
2023-12-13 12:11:26 +0100

    Nunchuck minigolf

--- Wii Nunchuck example/src/main.cpp
+++ Wii Nunchuck example/src/main.cpp
@@ -7,15 +7,18 @@
 #include <Arduino.h>
 
 // screen dimensions
-const int height = 240;
-const int width = 400;
+const int height = 400;
+const int width = 240;
+
+TKPoint pos(width / 2, height);
+TKPoint vel(0, 0);
 
 // Timing
 unsigned long lastFrame = 0;
 
 // SHARP display
 Adafruit_SharpMem display =
-    Adafruit_SharpMem(&SPI, PIN_LCD_CS, 400, 240, 8000000);
+    Adafruit_SharpMem(&SPI, PIN_LCD_CS, height, width, 8000000);
 
 // Wii Nunchuck
 WiiNunchuck chuck;
@@ -50,8 +53,23 @@ void pixelShow() {
 void setupDisplay() {
   SPI.begin(PIN_LCD_CLK, -1, PIN_LCD_DI, PIN_LCD_CS);
   display.begin();
-  display.setRotation(0);
+  display.setRotation(1);
   display.clearDisplay();
+}
+
+void boundsCheck() {
+  if (pos.x < 0) {
+    pos.x = 0;
+  }
+  if (pos.x > width) {
+    pos.x = width;
+  }
+  if (pos.y < 0) {
+    pos.y = 0;
+  }
+  if (pos.y > height) {
+    pos.y = height;
+  }
 }
 
 void setup() {
@@ -62,6 +80,7 @@ void setup() {
 
   setupDisplay();
 
+  // RGB LED show
   pixels.begin();
   pixelShow();
 
@@ -88,11 +107,16 @@ void loop() {
     if (frameTime > 0) {
       FPS = 1000 / frameTime;
     }
-
     lastFrame = now; // reset the timer
-    chuck.update();  // get data from nunchuck
+
+    // Wii
+    chuck.update(); // get data from nunchuck
+
+    display.clearDisplay();
+    // minigolf hole
+    display.fillCircle(width / 2, 30, 12, GRAY);
+
     if (chuck.c_button) {
-      display.clearDisplay();
       display.fillCircle(40, height / 4, 20, BLACK);
     }
     if (chuck.z_button) {
@@ -100,19 +124,25 @@ void loop() {
     }
 
     // chuck.printRaw(); // for debugging!
-
-    TKPoint cp(2 * chuck.joyX, -2 * chuck.joyY);
+    // TKPoint cp(2 * chuck.joyX, -2 * chuck.joyY);
     TKPoint cc(width / 2, height / 2);
-    cp = cp + cc;
-    // display.clearDisplay();  // clear the display
 
     // draw accelerometer positions
-    display.fillRect(400 / 4, 240 / 2, 4, chuck.aX - 128, GRAY);
-    display.fillRect(400 / 2, 240 / 2, 4, chuck.aY - 128, GRAY);
-    display.fillRect(3 * 400 / 4, 240 / 2, 4, chuck.aZ - 128, GRAY);
+    // display.fillRect(400 / 4, 240 / 2, 4, chuck.aX - 128, GRAY);
+    // display.fillRect(400 / 2, 240 / 2, 4, chuck.aY - 128, GRAY);
+    // display.fillRect(3 * 400 / 4, 240 / 2, 4, chuck.aZ - 128, GRAY);
 
+    TKPoint ap(chuck.aX - 128, -(chuck.aY - 128));
+    ap = ap + cc;
     // draw joystick position
-    display.fillCircle(cp.x, cp.y, 5, BLACK);
+    display.fillCircle(ap.x, ap.y, 5, GRAY);
+
+    // MAIN GUY
+    vel.set(chuck.aX - 128, -(chuck.aY - 128));
+    vel = vel * 0.1;
+    pos = pos + vel;
+    boundsCheck();
+    display.fillCircle(pos.x, pos.y, 10, BLACK);
 
     // draw center crosshair
     display.drawLine(0, height / 2, width, height / 2, BLACK);
--- Wii Nunchuck example/src/pins.h
+++ Wii Nunchuck example/src/pins.h
@@ -1,11 +1,9 @@
 #ifndef _PINS_PINS_H_
 #define _PINS_PINS_H_
 
-// #define WIDTH 400
-// #define HEIGHT 240
 // I2C pins
-#define PIN_SDA 1
-#define PIN_SCL 2
+#define PIN_SDA 2
+#define PIN_SCL 1
 
 
 // Colors
@@ -20,6 +18,5 @@
 #define PIN_LCD_CLK 12
 #define PIN_LCD_DI 13
 #define PIN_LCD_CS 14
-// #define PIN_ 11
 
 #endif
